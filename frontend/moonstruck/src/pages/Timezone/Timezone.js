dcff9b544788c0f9b4194eabf0ba89776c158a62
Shalini Rohra <shalini@s-128-97-87-252.resnet.ucla.edu>
2023-04-22 02:41:53 -0700

    tried adding timezone

--- frontend/moonstruck/src/pages/Timezone/Timezone.js
+++ frontend/moonstruck/src/pages/Timezone/Timezone.js
@@ -1,6 +1,35 @@
+import './Timezone.css'
+
 function Timezone (props) {
     return (
-        <h1>timezone</h1>
+        <>
+            <head>
+                <meta charset="utf-8"/>
+                <title>Glassmorphism Circle Progress</title>
+            </head>
+            <body>
+                <section>
+                    <div class="container">
+                        <div class="card">
+                            <div class="box">
+                                <div>
+                                    <div class="percent">
+                                        <svg>
+                                            <circle cx="70" cy="70" r="100"></circle>
+                                            <circle cx="70" cy="70" r="50"></circle>
+                                        </svg>
+                                        <div class="number">
+                                            <h2>7:00<span>am</span></h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text">Html</div>
+                        </div>
+                    </div>
+                </section>
+            </body>
+        </>
     )
 }
 
