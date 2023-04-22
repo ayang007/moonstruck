3c06390757ba51cb9423400c426e5d6c84ea1077
ayang007 <86279159+ayang007@users.noreply.github.com>
2023-04-22 12:24:11 -0700

    Pie charts

--- frontend/moonstruck/src/pages/Timezone/Timezone.js
+++ frontend/moonstruck/src/pages/Timezone/Timezone.js
@@ -2,26 +2,27 @@ import './Timezone.css'
 
 function Timezone (props) {
     return (
-        <div timezonecontainer>
-            <div class="container">
-                        <div class="card">
-                                <div>
-                                    <div class="percent">
-                                        <svg class="circlecontainer">
-                                            <circle cx="70" cy="70" r="100"></circle>
-                                            <circle cx="70" cy="70" r="100"></circle>
-                                            <circle cx="70" cy="70" r="100"></circle>
-                                        </svg>
-                                        <div class="number">
-                                            <h2>7:00<span>am</span>-1:00<span>pm</span></h2>
-                                        </div>
-                                    </div>
-                                </div>
-                        </div>
-                    </div>
-        </div>
 
+        <div class="timezone-out">
+            <div class="timezone-in">
+                <div class="pie" style={{"--p":40, "--s":"20deg", "--c": "#6616ad"}}> </div>
+                
+            </div>
+            <div class="timezone-in">
+                <div class="pie" style={{"--p":10, "--s":"10deg", "--c": "#4960e6"}}> </div>
+            </div>
+        </div>
     )
+    /*return (
+        <body class="timezonecontainer">
+            <div class="skill">
+                <div class="outer">
+                    <div class="inner"></div>
+                </div>
+            </div>
+        </body>
+
+    )*/
 }
 
 export default Timezone;
