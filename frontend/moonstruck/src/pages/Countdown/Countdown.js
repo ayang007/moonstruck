bb4b6ecd498ba9e55df2c94749a64b6d57e84cc9
Shalini Rohra <shalini@s-128-97-87-252.resnet.ucla.edu>
2023-04-22 04:00:38 -0700

    template for timer

--- frontend/moonstruck/src/pages/Countdown/Countdown.js
+++ frontend/moonstruck/src/pages/Countdown/Countdown.js
@@ -2,16 +2,17 @@ import './Countdown.css'
 
 function Countdown (props) {
     return (
-        <div class="countdowncontainer">
-        <h1 id="headline">Countdown to the next time we meet</h1>
-        <div id="countdown">
-            <ul>
-            <li><span id="Month"></span>days</li>
-            <li><span id="hours"></span>Hours</li>
-            <li><span id="minutes"></span>Minutes</li>
-            <li><span id="seconds"></span>Seconds</li>
-            </ul>
-        </div>
+        <div class="container">
+            <h1 id="headline">Countdown to the next time we meet</h1>
+            <div id="countdown">
+                <ul>
+                    <li><span id="months"></span>months</li>
+                    <li><span id="days"></span>days</li>
+                    <li><span id="hours"></span>hours</li>
+                    <li><span id="minutes"></span>minutes</li>
+                    <li><span id="seconds"></span>seconds</li>
+                </ul>
+            </div>
         </div>
     )
 }
