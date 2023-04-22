31c62ad98b012d06382eece6351129417e182470
ayang007 <86279159+ayang007@users.noreply.github.com>
2023-04-21 22:57:01 -0700

    basic structure

--- frontend/moonstruck/src/pages/Dashboard/Dashboard.js
+++ frontend/moonstruck/src/pages/Dashboard/Dashboard.js
@@ -4,4 +4,51 @@ function Dashboard (props) {
     )
 }
 
+function Fridge (props) {
+    return {
+        
+    }
+}
+
+function Timezone (props) {
+    return {
+        
+    }
+}
+
+function Weather (props) {
+    return {
+        
+    }
+}
+
+function Map (props) {
+    return {
+        
+    }
+}
+
+function PeriodCalendar (props) {
+    return {
+        
+    }
+}
+
+function Countdown (props) {
+    return {
+        
+    }
+}
+
+function Magpie (props) {
+    return {
+        
+    }
+}
+function PostIts (props) {
+    return {
+        
+    }
+}
+
 export default Dashboard;
