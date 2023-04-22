e914a12cfafc613448eaf74d71b8de3e8fbf765d
jaczhi <54284925+jaczhi@users.noreply.github.com>
2023-04-21 23:18:43 -0700

    global auth token react impl

--- frontend/moonstruck/src/pages/Login/Login.js
+++ frontend/moonstruck/src/pages/Login/Login.js
@@ -1,6 +1,14 @@
+import { useContext } from "react";
+import { AuthContext } from "../../App";
+
 function Login (props) {
+    const auth = useContext(AuthContext);
     return (
+        <>
         <h1>Login</h1>
+        <p>My token is {auth}</p>
+        <button onClick={() => {props.setAuth('bar')}}>Change to bar</button>
+        </>
     )
 }
 
